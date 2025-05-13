5f47345b31ebc46aa3ef8f6be8440c1f43d7d816
matejbozic57 <matej.bozic57@gmail.com>
2025-05-13 19:33:14 +0200

    trgovina.sql

---
+++ trgovina.sql
@@ -0,0 +1,21 @@
+﻿--create database trgovina;
+
+--use trgovina;
+
+--create table racuni(
+--sifra int,
+--broj varchar(10),
+--kupac varchar(100)
+--);
+
+--create table proizvodi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2)
+--);
+
+create table stavke(
+racun int,
+proizvod int,
+kolicina decimal(5,3)
+);
