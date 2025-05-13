d48111dabcb97dc36c45773f31f8ab3c4ad52698
matejbozic57 <matej.bozic57@gmail.com>
2025-05-13 18:42:10 +0200

    ERA ssms nastava

---
+++ skriptawp8.sql
@@ -0,0 +1,51 @@
+﻿
+-- ovo je komentar i ovo se ne izvodi
+
+-- Redosljed rada:
+-- 1. Pripremiti (napisati) upit
+-- 2. Izvesti upit (F5)
+-- 3. Provjeriti uspješnost izvođenja upita
+-- 4. Spremiti datoteku (ctrl + s)
+-- 5. Commit (i opcionalno push) na git
+-- 6. Na kraju rada opabvezno push na git
+--create database edunovawp8;
+
+--drop database edunovawp8;
+
+--use edunovawp8;
+
+-- nazivi tablica u množini zbog C# EF
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--datumpokretanja datetime,
+--aktivan bit
+--);
+
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--smjer int,
+--predavac varchar(50)
+--);
+
+--drop table polaznici;
+
+--create table polaznici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100)
+--);
+
+--create table clanovi(
+--grupa int,
+--polaznik int
+--);
+
+
+
+
+
