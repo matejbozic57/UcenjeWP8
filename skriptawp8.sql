34c5dfc8571739ff65ecb6df3187592ff9213732
matejbozic57 <matej.bozic57@gmail.com>
2025-05-13 19:58:12 +0200

    kopiranje

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -2,34 +2,28 @@
 -- ovo je komentar i ovo se ne izvodi
 use master;
 go
-drop database if exists edunovawp8
+drop database if exists edunovawp8;
 go
 create database edunovawp8;
 go
 use edunovawp8;
 
-
-
---nazivi tablica u množini zbog C# EF
-
+-- nazivi tablica u množini zbog C# EF
 create table smjerovi(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
-cijena decimal(18,2) null, --null se ne mora pisati
-datumpokretanja datetime, --i ovo je null iako ne pise 
+cijena decimal(18,2) null, -- null se ne mora pisati
+datumpokretanja datetime, -- i ovo je null iako ne piše
 aktivan bit not null default 0
 );
 
 
-
 create table grupe(
-sifra int not null primary key identity(1,1), 
-naziv varchar (20) not null,
+sifra int not null primary key identity(1,1),
+naziv varchar(20) not null,
 smjer int not null references smjerovi(sifra),
 predavac varchar(50)
 );
-
-
 
 create table polaznici(
 sifra int not null primary key identity(1,1),
@@ -39,9 +33,9 @@ email varchar(100)
 );
 
 create table clanovi(
---tablica nemora nuzno imati sifru
+-- tablica nužno ne mora imati šifru
 grupa int not null references grupe(sifra),
-polaznik int not null references polaznici(sifra),
+polaznik int not null references polaznici(sifra)
 );
 
 
--- trgovina.sql
+++ trgovina.sql
@@ -1,21 +1,25 @@
-﻿--create database trgovina;
+﻿use master;
+go
+drop database if exists trgovina;
+go
+create database trgovina;
+go
+use trgovina;
 
---use trgovina;
+create table racuni(
+sifra int not null primary key identity(1,1),
+broj varchar(10) not null,
+kupac varchar(100) 
+);
 
---create table racuni(
---sifra int,
---broj varchar(10),
---kupac varchar(100)
---);
-
---create table proizvodi(
---sifra int,
---naziv varchar(50),
---cijena decimal(18,2)
---);
+create table proizvodi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+cijena decimal(18,2) not null
+);
 
 create table stavke(
-racun int,
-proizvod int,
-kolicina decimal(5,3)
+racun int not null references racuni(sifra),
+proizvod int not null references proizvodi(sifra),
+kolicina decimal(5,3) not null
 );
