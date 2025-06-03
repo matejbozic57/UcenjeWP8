d0a14cd88d4ad66c88f442d5b110f80111869a1f
matejbozic57 <matej.bozic57@gmail.com>
2025-06-03 19:52:46 +0200

    UlazIzlaz

---
+++ Backend/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+
+        //unutar klase radimo metodu
+        public static void Izvedi()
+        {
+
+
+
+            Console.Write("Pozdrav Svijetu");
+
+            Console.WriteLine("isti red");
+
+        } // kraj metode
+
+
+
+
+
+    } //kraj klase
+} //kraj imenskog prostora (namespace)
--- Backend/Ucenje/Program.cs
+++ Backend/Ucenje/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+
+using Ucenje;
+
+E01UlazIzlaz.Izvedi();
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
