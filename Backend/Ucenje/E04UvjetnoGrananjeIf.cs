62d5d1a55c3870daec5655066910c4d89ebddbf3
matejbozic57 <matej.bozic57@gmail.com>
2025-06-08 10:47:05 +0200

    E04 ispravak

--- Backend/Ucenje/E04UvjetnoGrananjeIf.cs
+++ Backend/Ucenje/E04UvjetnoGrananjeIf.cs
@@ -1,7 +1,8 @@
-﻿
-
-using System.ComponentModel.Design;
-using System.Threading.Channels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Ucenje
 {
@@ -10,32 +11,121 @@ namespace Ucenje
 
         public static void Izvedi()
         {
-            Console.WriteLine("E04UvjetnoGrananjeI");
+            Console.WriteLine("E04UvjetnoGrananjeIf");
 
-            int broj = 7; //ovo je unio korisnik
 
-            // operator == provjerava jednakosti, rezultat je bool tip podatka
+            int broj = 17; // Ovo je unio korisnik
+
+            // operator ==   provjera jednakosti, rezultat je bool tip podatka
 
             bool uvjet = broj == 7;
 
-            //osnovna if sintaksa
+
+
+            // osnovna if sintaksa
             // if radi s bool tipom podatka
             if (uvjet)
             {
                 Console.WriteLine("Super");
-
             }
 
-            //if ne mora imati else
-            // if ne mora imati {} ali kada ih nema odnosi se samo na prvu sljedecu liniju
+
+
+            // if ne mora imati else
+            // if ne mora imati {} ali kada ih nema odnosi se samo na prvu sljedeću liniju
 
             // puna sintaksa if naredbe
 
-       
-   
-            
-            
+            if (broj > 2)
+            {
+                Console.WriteLine("OK");
+            }
+            else if (broj == 1)
+            {
+                Console.WriteLine("Nije dobro");
+            }
+            else
+            {
+                Console.WriteLine("NIJE OCJENA");
+            }
+
+
+
+            // operatori < >, >=, <=, != se koriste u if-u
+
+
+            // logički operatori: and, or i not
+
+            // and
+            // & (shift+6)
+
+
+            int i = 5, j = 10;
+
+            // prvi uvjet ne prolazi ALI se drugi uvjet gleda
+            if (i > 10 & j > 2)
+            {
+                Console.WriteLine("Neće se ispisati");
+            }
+
+            // INAČE KAKO SE KORISTI
+            // AKO prvi uvjet ne prođe, ne gleda se drugi
+            if (i > 10 && j > 2)
+            {
+                Console.WriteLine("Neće se ispisati");
+            }
+
+
+            // or
+            // | (altgr+w)
+            // AKO prvi uvjet prođe ne gleda se drugi
+            if (i == 4 || j == 2)
+            {
+                Console.WriteLine("Jedno od dvoje");
+            }
+
+
+            // not 
+            // !
+
+            if (!uvjet)
+            {
+                Console.WriteLine("Kada gore ne ispiše, ovdje će");
+            }
+
+
+
+            string grad = "Osijek";
+
+            if (uvjet && i > 10)
+            {
+                if (grad == "Vukovar")
+                {
+                    Console.WriteLine("Novi prvoligaš");
+                }
+            }
+
+
+            if (grad == "Osijek")
+            {
+                Console.WriteLine("Super");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
+
+
+
+
+            // inline if
+
+            Console.WriteLine(grad == "Osijek" ? "Super" : "OK");
+
+
+
         }
+
 
     }
 }
--- Backend/Ucenje/Program.cs
+++ Backend/Ucenje/Program.cs
@@ -1,21 +1,6 @@
 ﻿
 
-
-
 using Ucenje;
 
 
-
-
 E04UvjetnoGrananjeIf.Izvedi();
-
-   // E02Varijable.Izvedi();
-    //E01UlazIzlaz.Izvedi();
-
-
-
-
-
-
-    // See https://aka.ms/new-console-template for more information
-    //Console.WriteLine("Hello, World!");
